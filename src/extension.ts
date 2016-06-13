4fb9de39c39fbb611758ecddce3295b2b860e4f4
Arquimidez Mora <arquimidez.mora@dfacture.com>
2016-06-13 07:03:16 -0500

    Correcting process of saving and now show errors from salesforce

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,8 @@ export function activate(context: vscode.ExtensionContext) {
     // The commandId parameter must match the command field in package.json
     var output = vscode.window.createOutputChannel("Information");
     var active = vscode.window.activeTextEditor;
-    var Force = new ForceCaller(output, active);
+    var workspace = vscode.workspace;
+    var Force = new ForceCaller(output, active, workspace);
     let disposable = vscode.commands.registerCommand('ForceCaller.connect', () => {
         // The code you place here will be executed every time your command is executed
         if(Force.org == ''){
@@ -25,19 +26,14 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(disposable);
 
-    context.subscriptions.push(vscode.commands.registerCommand('ForceCaller.getInfo', () => {
-        // The code you place here will be executed every time your command is executed
-        Force.getInfo();
-    }));
-
-    context.subscriptions.push(vscode.commands.registerCommand('ForceCaller.getFile', () => {
-        // The code you place here will be executed every time your command is executed
-        Force.getFile();
-    }));
-
     context.subscriptions.push(vscode.commands.registerCommand('ForceCaller.saveFile', () => {
         // The code you place here will be executed every time your command is executed
         Force.saveFile();
+    }));
+    
+    context.subscriptions.push(vscode.commands.registerCommand('ForceCaller.createContainer', () => {
+        // The code you place here will be executed every time your command is executed
+        Force.createMetadataContainer();
     }));
     
 }
@@ -50,21 +46,26 @@ class ForceCaller {
     private active;
     private salesforce;
     private checkStatusInterval;
+    private workspace;
+    private metadataContainer;
     public org = '';
 
-    constructor(output, active) {
+    constructor(output, active, workspace) {
         this.output = output;
         this.active = active;
+        this.workspace = workspace;
+        var setting = workspace.getConfiguration("ForceCaller.setting");
         this.salesforce = new jsforce.Connection({
             // you can change loginUrl to connect to sandbox or prerelease env.
-            loginUrl : 'https://test.salesforce.com'
+            loginUrl : setting.get("host")
         });
     }
 
     public connect(){
         var self = this;
-        self.write('Connecting to salesforce....' + "\n");
-        this.salesforce.login('adminblein@itesm.mx.test', 'tecmty01$', function(err, res) {
+        var setting = self.workspace.getConfiguration("ForceCaller.setting");
+        self.write('Connecting to salesforce at '+setting.get("host")+' ....' + "\n");
+        this.salesforce.login(setting.get('user'), setting.get('password'), function(err, res) {
             if (err) { return console.error(err); }
             self.write("Conectado a la organizacion: "+res.organizationId + "\n");
             self.org = res.organizationId;
@@ -101,6 +102,8 @@ class ForceCaller {
     public saveFile()
     {
         var self = this;
+        var setting = self.workspace.getConfiguration("ForceCaller.setting");
+        var metadataContainerId = setting.get('MetadataContainer');
         self.write("Org: " + self.org + '\n');
         console.log(self.active);
         var newBody = self.active.document.getText();
@@ -120,45 +123,99 @@ class ForceCaller {
             console.log("fetched : " + records.length);
             for (var i=0; i < records.length; i++) {
                 var record = records[i];
-                console.log('Name: ' + record.Name);
-                /*self.salesforce.tooling.sobject('MetadataContainer').create({Name: 'SaveFromVsCode'+record.Id},function(err, res){
-                    if (err) { self.write(err);return console.error(err); }
-                    console.log(res);
-                });*/
+                console.log('Name: ' + record.Name);                                
                 self.salesforce.tooling.sobject(apexMember).create({
-                    MetadataContainerId: '1dc17000000IDLhAAO',
+                    MetadataContainerId: metadataContainerId,
                     ContentEntityId: record.Id,
                     Body: newBody
                 },function(err, res){
-                    if (err) { self.write(err);return console.error(err); }
-                    console.log(res);
-                    self.salesforce.tooling.sobject('ContainerAsyncRequest').create({
-                        MetadataContainerId: '1dc17000000IDLhAAO',
-                        isCheckOnly: false,
-                    },function(err, res){
-                        if (err) { self.write(err);return console.error(err); }else{
-                            var async = res;
-                            console.log(async);
-                            self.write("Saving to Salesforce ....\n");
-                            self.checkStatusInterval = setInterval(function(){
-                                self.salesforce.tooling.sobject('ContainerAsyncRequest').retrieve(async.id, function(err, res){
-                                    if (err) { self.write(err);return console.error(err); }else{
-                                        if(res.State != 'Queued'){
-                                            self.stopInterval();
-                                            self.write("File Saved!!\n");
-                                        }
-                                        else{
-                                            self.write("Waiting Salesforce response ....\n");
-                                        }
-                                    }                                                                      
-                                });
-                            }, 1000);
-                        }
-                    });
+                    if (err) { self.write('Error at '+apexMember+'\n');console.error('Error at '+apexMember+'\n' );return console.error(err); }else{
+                        console.log(err);
+                        console.log(res);
+                        self.salesforce.tooling.sobject('ContainerAsyncRequest').create({
+                            MetadataContainerId: metadataContainerId,
+                            isCheckOnly: false,
+                        },function(err, res){
+                            if (err) { self.write('Error at creating ContainerAsyncRequest\n');console.error('Error at creating ContainerAsyncRequest\n' );return console.log(err); }else{
+                                var async = res;
+                                console.log(async);
+                                self.write("Saving to Salesforce file '"+fileName+"."+ext+"'....\n");
+                                self.checkAsyncStatus(async.id);
+                            }
+                        });
+                    }
                 });
             }
         });
-        
+    }
+    
+    public checkAsyncStatus(AsyncId){
+        var self = this;
+        self.write("Waiting Salesforce response ....\n");
+        self.salesforce.tooling.sobject('ContainerAsyncRequest').retrieve(AsyncId, function(err, res){                                    
+            if (err) { self.write('Error at retrieve ContainerAsyncRequest\n' );console.error('Error at retrieve ContainerAsyncRequest\n' );return console.log(err); }else{
+                if(res.State != 'Queued'){
+                    console.log("Estado del guardado: ");
+                    console.log(res);
+                    if(res.ErrorMsg != null){
+                        self.write(res.ErrorMsg + "\n");                                                
+                    }
+                    else{
+                        if(res.State == 'Failed'){
+                            var messages = res.DeployDetails.componentFailures;
+                            for(var i = 0; i < messages.length; i++){
+                                var msg = messages[i];
+                                self.write("**** There is an error with the " + msg.componentType + " " + "'" + msg.fullName + "' at" + "\n");
+                                self.write("**** Line Number: " + msg.lineNumber + ", column: " + msg.columnNumber + "\n");
+                                self.write("**** Error Msg: " + msg.problem + "\n");
+                            }
+                        }
+                        else{
+                            self.write("File Saved!!\n");    
+                        }                            
+                    }                                            
+                }
+                else{
+                    self.checkAsyncStatus(AsyncId);
+                }
+            }                                                                      
+        });
+    }
+    
+    public createMetadataContainer(){
+        var self = this;
+        self.salesforce.tooling.sobject('MetadataContainer')
+        .find({ Name: 'SaveFromVsCode' })
+        .execute(function(err, records) {
+            if (err) { return console.error(err); }
+            console.log(records);
+            console.log("fetched : " + records.length);
+            if(records.length > 0) {
+                self.deleteMetadataContainer();
+                self.createContainer();
+            }
+            else{
+                self.createContainer();
+            }
+        });
+    }
+    
+    public createContainer(){
+        var self = this;
+        self.salesforce.tooling.sobject('MetadataContainer').create({Name: 'SaveFromVsCode'},function(err, res){
+            if (err) { self.write("There was an error creating the MetadataContainer\n");return console.error(err); }
+            console.log(res);
+            self.write("Creado contenedor: "+res.id + "\n\n");
+        });
+    }
+    
+    public deleteMetadataContainer(){
+        var self = this;
+        var setting = self.workspace.getConfiguration("ForceCaller.setting");
+        self.salesforce.tooling.sobject('MetadataContainer').delete(setting.get('MetadataContainer'),function(err, res){
+            if (err) { self.write("There was an error deleting the MetadataContainer\n");return console.error(err); }
+            console.log(res);
+        });
     }
 
     public stopInterval(){
